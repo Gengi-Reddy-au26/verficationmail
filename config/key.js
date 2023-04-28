3e33371b3782c067f546a3e341859d5ddc565d84
Gangareddy N <88926606+Gengi-Reddy-au26@users.noreply.github.com>
2023-04-28 13:57:33 +0530

    Add files via upload

---
+++ config/key.js
@@ -0,0 +1,5 @@
+module.exports = {
+    // MongoURI: "mongodb+srv://Gangareddy:Reddy12@cluster0.hjhorxm.mongodb.net/?retryWrites=true&w=majority"
+
+    MongoURI: "mongodb+srv://Gangareddy:Reddy12@cluster0.hjhorxm.mongodb.net/?retryWrites=true&w=majority"
+}
